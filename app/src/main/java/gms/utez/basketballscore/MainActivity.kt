8e0905c209a7a9fb0dc3b356e1fa03437c89acbc
Gustavo <20203tn156@utez.edu.mx>
2021-11-03 21:19:12 -0600

    Suma, resta y reinicia puntos.

--- app/src/main/java/gms/utez/basketballscore/MainActivity.kt
+++ app/src/main/java/gms/utez/basketballscore/MainActivity.kt
@@ -2,10 +2,66 @@ package gms.utez.basketballscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var pointsLocalX = 0
+        var pointsVisitorX = 0
+
+        //Local
+        btnMas1Local.setOnClickListener{
+            pointsLocalX = pointsLocal.text.toString().toInt()
+            pointsLocalX = pointsLocalX + 1
+            pointsLocal.text = pointsLocalX.toString()
+        }
+
+        btnMas2Local.setOnClickListener {
+            pointsLocalX = pointsLocal.text.toString().toInt()
+            pointsLocalX = pointsLocalX + 2
+            pointsLocal.text = pointsLocalX.toString()
+        }
+
+        btnMenos1Local.setOnClickListener {
+            pointsLocalX = pointsLocal.text.toString().toInt()
+            if (pointsLocalX == 0){
+                Toast.makeText(this, "Chuco, no se puede hacer eso",  Toast.LENGTH_LONG).show()
+            } else{
+                pointsLocalX = pointsLocalX -1
+                pointsLocal.text = pointsLocalX.toString()
+            }
+
+        }
+        //Visitante
+
+        btnMas1Visitor.setOnClickListener{
+            pointsVisitorX = pointsVisitor.text.toString().toInt()
+            pointsVisitorX = pointsVisitorX + 1
+            pointsVisitor.text = pointsVisitorX.toString()
+        }
+
+        btnMas2Visitor.setOnClickListener {
+            pointsVisitorX = pointsVisitor.text.toString().toInt()
+            pointsVisitorX = pointsVisitorX + 2
+            pointsVisitor.text = pointsVisitorX.toString()
+        }
+
+        btnMenos1Visitor.setOnClickListener {
+            pointsVisitorX = pointsVisitor.text.toString().toInt()
+            if (pointsVisitorX == 0){
+                Toast.makeText(this, "Chuco, no se puede hacer eso",  Toast.LENGTH_LONG).show()
+            } else{
+                pointsVisitorX = pointsVisitorX - 1
+                pointsVisitor.text = pointsVisitorX.toString()
+            }
+
+        }
+        btnReinicio.setOnClickListener{
+            pointsLocal.text = "0"
+            pointsVisitor.text = "0"
+        }
     }
 }
